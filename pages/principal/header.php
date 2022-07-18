60ce701a7b29dd01f9a1f9610dea3fda200f31d7
KengiiGMs <kaikiaoto22@gmail.com>
2022-07-18 19:11:48 -0300

    pagina Home resposiva

--- pages/principal/header.php
+++ pages/principal/header.php
@@ -1,5 +1,5 @@
 <header>
-    <nav class="navbar navbar-expand-lg bg-green navbar-fixed-top">
+    <nav class="navbar navbar-expand-lg" style=" background-color: rgb(36, 133, 65);">
         <div class="container-fluid">
             <a class="navbar-brand" href="home.php"><img src='../../imgs/logo2.png' width="60"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -10,54 +10,31 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="indexpessoa.php" style="color:white;">CADASTRO</a>
+                        <a class="nav-link " href="indexpessoa.php" style="color:white">FAMILIAS</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="indexcestas.php" style="color:white;">CESTAS</a>
+                        <a class="nav-link" href="indexcestas.php" style="color:white">CESTAS</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="indexconta.php" style="color:white;">CONTA</a>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false" style="color:white">
+                            CONTA
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown"
+                            style=" background-color: rgb(36, 133, 65);">
+                            <li><a class="dropdown-item" href="indexconta.php" style="color:white">VER PERFIL</a>
+                            </li>
+
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="../../indexlogin.php" style="color:white">SAIR</a>
+                            </li>
+                        </ul>
                     </li>
                 </ul>
+
             </div>
-            <a id='logoutA' href="../../indexlogin.php">
-                <img id='logout' src="../../imgs/conta/logout.png">
-            </a>
         </div>
     </nav>
 </header>
-
-
-
-<!-- <nav>
-        <ul>
-            <a href="home.php">
-                <img id='logoNav' src='../../imgs/logo2.png'>
-            </a>
-
-            <li>
-                <a href="indexpessoa.php">
-                    CADASTRO
-                </a>
-            </li>
-
-            <li>
-                <a href="indexcestas.php">
-                    CESTAS
-                </a>
-            </li>
-
-            <li>
-                <a href="indexconta.php">
-                    CONTA
-                </a>
-            </li>
-
-            <ul id="logoutUl">
-
-                <a id='logoutA' href="../../indexlogin.php">
-                    <img id='logout' src="../../imgs/conta/logout.png">
-                </a>
-            </ul>
-        </ul>
-    </nav> -->
--- pages/principal/home.php
+++ pages/principal/home.php
@@ -1,73 +1,95 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Inicio</title>
 
     <script src="https://code.jquery.com/jquery-3.3.1.js"
         integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous">
     </script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <script>
     $(function() {
         $("#header").load("header.php");
     });
     </script>
-
     <style>
-    <?php include '../style.css';
-    ?>
+    .buttonH {
+        margin-top: 20px;
+        border: none;
+        color: green;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 18px;
+        border-radius: 10px;
+        border-style: solid;
+        border-color: green;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        transition-duration: 0.4s;
+    }
+
+    .buttonH:hover {
+        background-color: green;
+        color: white;
+    }
     </style>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 
-<header id="header"></header>
 
 <body>
+    <header id="header"></header>
+    <div class="container"
+        style="background-image: linear-gradient( green, yellow); margin-top:50px; border-radius:20px; border-style:solid; border-color:green; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+        <div class="container-fluid">
+            <div class="col">
+                <div class="row" style="text-align:center">
+                    <div><img src=" ../../imgs/Logo.png" alt="logo" class="img-fluid"></div>
+                </div>
+            </div>
+        </div>
 
-    <div class=caixa id="caixaHome">
-
-        <img id='logo' src="../../imgs/Logo.png" alt="logo">
-
-        <h2 id='subTitleHome'>Bem-Vindo!
-            <br>O que deseja acessar?
-        </h2>
-
-        <table>
-            <form action='indexpessoa.php'>
-                <td class='botoesHome'>
-                    <p><input type='submit' value="Cadastro"></p>
-                </td>
-            </form>
-
-            <form action='indexcestas.php'>
-                <td class='botoesHome'>
-                    <p><input type='submit' value="Cestas"></p>
-                </td>
-            </form>
-
-            <form action='indexconta.php'>
-                <td class='botoesHome'>
-                    <p><input type='submit' value="Conta"></p>
-                </td>
-            </form>
-
-            </form>
-        </table>
-
-
-
-
+        <div class=" container" style=" border-radius:0 0 20px 20px;">
+            <div class="col">
+                <div class="row">
+                    <p style="text-align:center; color:green; margin-top:5px; font-size:20px"><b>Bem-Vindo!
+                            <br>O que deseja acessar?
+                    </p></b>
+                </div>
+            </div>
+        </div>
     </div>
+    <div class="container" style="margin-bottom:10px">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
-    </script>
-</body>
+        <div class="row" style="text-align:center">
+            <div class="col">
+                <div class="container"><button type="button" class="buttonH"
+                        onclick="window.location.href='indexpessoa.php' ">Familias</button>
+                </div>
+            </div>
+            <div class="col">
+                <div class="container"><button type="button" class="buttonH"
+                        onclick="window.location.href='indexcestas.php'">Cestas</button>
+                </div>
+            </div>
+            <div class="col">
+                <div class="container"> <button type="button" class="buttonH"
+                        onclick="window.location.href='indexconta.php'">Contas</button>
+                </div>
+            </div>
 
-<?php include('footer.php');?>
+
+
+        </div>
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+        </script>
+
 
 </html>
