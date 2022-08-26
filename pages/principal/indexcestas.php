ea0861c0b4b68e5db54424a33665e78b350b7d32
matheus7p <matheus.costa.7p@gmail.com>
2022-08-26 17:02:40 -0300

    modificação na tela de cesta, modal e ações

--- pages/principal/indexcestas.php
+++ pages/principal/indexcestas.php
@@ -44,15 +44,15 @@
                                 <div class="modal-body">
                                     <form action='../../crud/cestas/controlecestas.php' method='GET'>
                                         <p>
-                                            <input class="inputModalCadastro" type="number" min="0" name="idCestas"
+                                            <input class="inputModalCadastroCesta" type="number" min="0" name="idCestas"
                                                 placeholder="Id" />
                                         </p>
                                         <p>
-                                            <input class="inputModalCadastro" type="text" name="quantidadeCestas"
+                                            <input class="inputModalCadastroCesta" type="text" name="quantidadeCestas"
                                                 placeholder="Quantidade" />
                                         </p>
                                         <p>
-                                            <input class="inputModalCadastro" type="date" name="recebimentoCestas"
+                                            <input id="inputDataCadastroCesta" type="date" name="recebimentoCestas"
                                                 placeholder="Data de Recebimento" />
                                         </p>
                                 </div>
@@ -102,22 +102,19 @@
                         echo "<td style='text-align:center'>".$row['quantidade_cestas']."</td>";
                         echo "<td style='text-align:center'>".$row['recebimento_cestas']."</td>";
                         echo "<td>
-                            <a class='btn btn-sm btn-warning' href='indexcestasEdit.php?idCestas=$row[idCestas]'><svg
+                            <a class='btn btn-outline-success btn-sm ' href='indexcestasEdit.php?idCestas=$row[idCestas]'><svg
                                     xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor'
                                     class='bi bi-pencil' viewBox='0 0 16 16'>
                                     <path
                                         d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z' />
                                 </svg>
                             </a>
-                            <a class='btn btn-sm btn-danger'
+                            <a class='btn btn-sm btn-outline-danger'
                             data-bs-toggle='modal' data-bs-target='#staticBackdrop'>
-                                <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor'
-                                    class='bi bi-file-earmark-excel' viewBox='0 0 16 16'>
-                                    <path
-                                        d='M5.884 6.68a.5.5 0 1 0-.768.64L7.349 10l-2.233 2.68a.5.5 0 0 0 .768.64L8 10.781l2.116 2.54a.5.5 0 0 0 .768-.641L8.651 10l2.233-2.68a.5.5 0 0 0-.768-.64L8 9.219l-2.116-2.54z' />
-                                    <path
-                                        d='M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z' />
-                                </svg>
+                            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>
+                              <path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z'/>
+                              <path fill-rule='evenodd' d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z'/>
+                            </svg>
                             </a>
                             <div class='modal fade' id='staticBackdrop' data-bs-backdrop='static' data-bs-keyboard='false' tabindex='-1' aria-labelledby='staticBackdropLabel' aria-hidden='true'>
                             <div class='modal-dialog'>
