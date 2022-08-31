9fb269a79389a10552bebbc1259efdc2d8ad3c86
KengiiGMs <kaikiaoto22@gmail.com>
2022-08-31 11:39:56 -0300

    add Cards na Tela home

--- pages/principal/home.php
+++ pages/principal/home.php
@@ -5,8 +5,15 @@
         unset($_SESSION['usuario']);
         unset($_SESSION['senha']);
         header('location: ../../indexlogin.php');
+    }else{
+        $logado = $_SESSION['usuario'];
+        include_once('../../connection/conexao.php');
+        $banco = new conexao();
+        $con = $banco->getConexao();
+        $contF = $con->query('select count(*) from familia')->fetchColumn(); 
+        $contC = $con->query('select count(*) from cestas')->fetchColumn(); 
+        
     }
-    $logado = $_SESSION['usuario'];
 
 
 ?>
@@ -49,7 +56,7 @@
             <div class="col-sm-4">
 
                 <div class=" card" id="cardContainer">
-                    <a href="indexpessoa.php" style="text-decoration: none;">
+                    <a href="indexfamilia.php" style="text-decoration: none;">
                         <div class="container">
                             <img src="../../imgs/iconesMenu/Familia.png" class="card-img-top" alt="Familia">
                         </div>
@@ -89,6 +96,38 @@
                 </div>
             </div>
 
+        </div>
+        <div class="row">
+            <div class="container" id="containerInfoFam">
+                <div class="jumbotron jumbotron-fluid">
+                    <h1 class="display-4" style="color:green">Familias</h1>
+                    <p class="lead">
+                        <?php
+                            echo"<p><b>Total de Familias Cadastradas: $contF</b></p>";
+                        ?>
+                    </p>
+                    <hr class="my-4">
+                    <p class="lead">
+                        <a class="btn btn-success btn-lg" href="#" role="button">Ver Mais</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="container" id="containerInfoFam">
+                <div class="jumbotron jumbotron-fluid">
+                    <h1 class="display-4" style="color:green">Cestas</h1>
+                    <p class="lead">
+                        <?php
+                            echo"<p><b>Total de Cestas Cadastradas: $contC</b></p>";
+                        ?>
+                    </p>
+                    <hr class="my-4">
+                    <p class="lead">
+                        <a class="btn btn-success btn-lg" href="#" role="button">Ver Mais</a>
+                    </p>
+                </div>
+            </div>
         </div>
 
     </div>
