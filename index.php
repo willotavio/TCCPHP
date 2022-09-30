f68c9e1aa6af58d465a30351c969ed55a18f2aa5
willotavio <willian.oliveira177@etec.sp.gov.br>
2022-09-30 17:06:43 -0300

    adicionada navs e tabs para login e recuperação de senha

--- index.php
+++ index.php
@@ -18,7 +18,19 @@
 
 <body style="background-color: whitesmoke;">
 
-    <div class="container" style="margin-top:10%">
+<div style="margin-top: 200px; text-align: center;">
+<ul class="nav nav-pills white justify-content-center" id="myTab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true" style="color: green">Login</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false" style="color: green">Esqueci Minha Senha</button>
+  </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+    
+  <div class="container" style="width: 600px">
         <div class=" col-10 m-auto">
             <div class="row">
                 <div class=" col-md-11 m-auto">
@@ -38,10 +50,6 @@
                                     <label>Senha</label>
                                 </div>
                                 <div class="container" style="margin:10px">
-                                    <div style="text-align:right">
-                                        <a href="pages/login/esqueciSenha.php" class="linksIndexLogin">Esqueci
-                                            Minha Senha<a>
-                                    </div>
                                 </div>
                                 <div class="row m-auto">
                                     <button type="submit" class="btn btn-success btn-lg btn-block"
@@ -60,6 +68,43 @@
             </div>
         </div>
     </div>
+
+  </div>
+  <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
+    
+  <div class="container"  style="width: 600px">
+        <div class=" col-10 m-auto">
+            <div class="row">
+                <div class=" col-md-11 m-auto">
+                    <div id="containerEsqueciSenha">
+                        <div class="container mt-3">
+                            <form method="GET" action="../../crud/criarConta/controleCriarConta.php" autocomplete="off">
+                                <h1 style="text-align:center; font-size:25px; padding:15px; color:rgba(25,135,84,255)">
+                                    ESQUECI MINHA SENHA</h1>
+                                <div class="form-floating mb-3 mt-3">
+                                    <input type="email" class="form-control" placeholder="Digite o seu Usuario" required
+                                        name="cadastrarUEmail" id="email_recuperacao">
+                                    <label>Digite o seu Email</label>
+                                </div>
+
+                                <div class="row m-auto">
+                                    <button type="submit" class="btn btn-success btn-lg btn-block"
+                                        style="font-size:16px" value="cadastrar">Enviar</button>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+  </div>
+</div>
+</div>
+
+
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
     </script>
