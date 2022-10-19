feb207d7e47addbe40f7227c90faf9f8cb2111a7
willa0o <willaotavio@gmail.com>
2022-10-19 20:16:54 -0300

    Alteração no ícone de relatório de cestas

--- pages/cestas/relatorioCestas.php
+++ pages/cestas/relatorioCestas.php
@@ -100,7 +100,11 @@
         <button type="button" class="btn btn-outline-success" data-bs-toggle="modal"
             onclick="window.location.href='pdfCestas/pdfCestas.php';"
             style="font-size: 1.2em; width: 200px; margin-bottom: 10px">Gerar Relatório
-            <img src="../../imgs/iconesCestas/relatorioCestas.png" alt="relatorioCestas" width="50px"></button>
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-file-earmark-text" viewBox="0 0 16 16">
+                <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
+                <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5L9.5 0zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
+            </svg>
+        </button>
         <h4 style="color:green; text-align:center">Entrada de Cestas</h4>
         <div class="overflow-auto">
             <div class="column">
